484a48a18d17dc34953ee572cc8c3be28a468287
Miller <farwes1180@gmail.com>
2022-11-01 18:51:49 +0900

    FIX

--- basic_concept/app.js
+++ basic_concept/app.js
@@ -1,6 +1,52 @@
 const http = require("http");
 const requestHandler = require("./routes");
 
-const server = http.createServer(requestHandler);
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  // console.log(req.url, req.method, req.headers);
+  // process.exit();
+
+  const url = req.url;
+  const method = req.method;
+
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'></input><button type='submit'>SUBMIT</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      // fs.writeFileSync("message.txt", message); // sync "codeblock"
+      fs.writeFile("message.txt", message, (error) => {
+        console.log(error);
+      });
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+
+  res.write("<html>");
+  res.write("<head><title>First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+
+  res.end();
+});
 
 server.listen(3000);
