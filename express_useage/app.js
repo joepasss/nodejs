a20b9a110fd80f682a3c2ff263340497e523c49e
joepasss@linux <farwes1180@gmail.com>
2022-11-03 01:23:01 +0900

    middleware

--- express_useage/app.js
+++ express_useage/app.js
@@ -1,5 +1,20 @@
 const http = require("http");
 const express = require("express");
 
-const server = http.createServer();
+const app = express();
+
+app.use((req, res, next) => {
+	console.log("in the middleware!");
+	next(); // Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+	console.log("IN ANOTHER MIDDLEWARE!");
+
+	// html tag send!
+	res.send("<h1>Hello from express!</h1>");
+});
+
+const server = http.createServer(app);
+
 server.listen(3000);
