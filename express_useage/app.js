02eed9b0f86738207a842ba7dd027dd0038492f3
joepasss@linux <farwes1180@gmail.com>
2022-11-04 06:01:10 +0900

    use body parser & use http request method

--- express_useage/app.js
+++ express_useage/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/add-product", (req, res, next) => {
 	res.send(`
 	<h1>"/add-product" route</h1>
+	
+	<form action="/product" method="POST">
+		<input type="text" name="title" />
+		<button type="submit">Add Product</button>
+	</form>
 	`);
 });
 
-app.use("/", (req, res, next) => {
+app.post("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
+});
+
+app.get("/", (req, res, next) => {
 	res.send(`
 	<h1>"/" route</h1>
 	`);
