1af1819186bf6b0188b047091d3da814a18dc984
joepasss@linux <farwes1180@gmail.com>
2022-11-03 19:13:27 +0900

    body-parser

--- express_useage/app.js
+++ express_useage/app.js
@@ -1,15 +1,27 @@
 const http = require("http");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-	console.log("This is always run!");
-	next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-	res.send("<h1>ADD PRODUCT ROUTE!</h1>");
+	res.send(
+		`
+		<h1>ADD PRODUCT PAGE</h1>
+
+		<form action='/product' method="POST">
+			<input type='text' name='title' />
+			<button type='submit'>add product</button>
+		</form>
+		`
+	);
+});
+
+app.use("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
