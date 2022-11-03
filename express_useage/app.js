0d8586f75f4c6e52c75bbd4ba364b89c3c5e9b61
joepasss@linux <farwes1180@gmail.com>
2022-11-04 05:56:54 +0900

    use other route

--- express_useage/app.js
+++ express_useage/app.js
@@ -2,15 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("in the middleware");
-	next();
+app.use("/add-product", (req, res, next) => {
+	res.send(`
+	<h1>"/add-product" route</h1>
+	`);
 });
 
-app.use((req, res, next) => {
-	console.log("in Another middleware!");
-
-	res.send("<h1>HELLO FROM EXPRESS!</h1>");
+app.use("/", (req, res, next) => {
+	res.send(`
+	<h1>"/" route</h1>
+	`);
 });
 
 app.listen(3000);
