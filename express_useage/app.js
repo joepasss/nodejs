035cfe7bd537f97371365fdf85cdbbcae862d908
joepasss@linux <farwes1180@gmail.com>
2022-11-04 05:54:04 +0900

    middleware

--- express_useage/app.js
+++ express_useage/app.js
@@ -1,1 +1,16 @@
+const express = require("express");
 
+const app = express();
+
+app.use((req, res, next) => {
+	console.log("in the middleware");
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log("in Another middleware!");
+
+	res.send("<h1>HELLO FROM EXPRESS!</h1>");
+});
+
+app.listen(3000);
