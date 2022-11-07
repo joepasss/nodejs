60750f625ee0d7bd38b9d8b5ce26c90cb0a6af7d
Miller <farwes1180@gmail.com>
2022-11-08 07:55:18 +0900

    service html page

--- express_useage/app.js
+++ express_useage/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require("./util/path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,9 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).send(`
-	<h1>Page not found!</h1>
-	`);
+  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
 });
 
 app.listen(3000);
--- express_useage/routes/admin.js
+++ express_useage/routes/admin.js
@@ -1,21 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
+const rootDir = require("../util/path");
 
 router.post("/product", (req, res, next) => {
-	console.log(req.body);
-	res.redirect("/");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 router.get("/add-product", (req, res, next) => {
-	res.send(`
-	<h1>"/add-product" route</h1>
-	
-	<form action="/admin/product" method="POST">
-		<input type="text" name="title" />
-		<button type="submit">Add Product</button>
-	</form>
-	`);
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 module.exports = router;
