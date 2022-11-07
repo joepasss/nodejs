69cbbab2a8bb87510c3c2d235329fc077427d342
joepasss@linux <farwes1180@gmail.com>
2022-11-07 17:57:11 +0900

    filtering path

--- express_useage/routes/admin.js
+++ express_useage/routes/admin.js
@@ -2,18 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+// admin/add-product => GET
+
 router.get("/add-product", (req, res, next) => {
 	res.send(
 		`
 		<h1>ADD PRODUCT PAGE</h1>
 
-		<form action='/product' method="POST">
+		<form action='/admin/product' method="POST">
 			<input type='text' name='title' />
 			<button type='submit'>add product</button>
 		</form>
 		`
 	);
 });
+
+// admit/product => POST
 
 router.post("/product", (req, res, next) => {
 	console.log(req.body);
