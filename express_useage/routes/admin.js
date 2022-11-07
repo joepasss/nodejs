55184f03b39c75bec326f3b539b0a43b72571711
Miller <farwes1180@gmail.com>
2022-11-07 18:29:56 +0900

    service html page

--- express_useage/routes/admin.js
+++ express_useage/routes/admin.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
@@ -5,23 +6,14 @@ const router = express.Router();
 // admin/add-product => GET
 
 router.get("/add-product", (req, res, next) => {
-	res.send(
-		`
-		<h1>ADD PRODUCT PAGE</h1>
-
-		<form action='/admin/product' method="POST">
-			<input type='text' name='title' />
-			<button type='submit'>add product</button>
-		</form>
-		`
-	);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // admit/product => POST
 
 router.post("/product", (req, res, next) => {
-	console.log(req.body);
-	res.redirect("/");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 module.exports = router;
